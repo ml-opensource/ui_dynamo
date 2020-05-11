fcd823bc4405f663731f8c27fee4dd75bbc1a7d1
fuzzagrosner <andrew@fuzzproductions.com>
2020-05-11 17:38:36 -0400

    [MediaQuery] allow left to overflow, but not top.

--- lib/ui/storyboard/utils.dart
+++ lib/ui/storyboard/utils.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
-double calculateLeft(Offset offset, Offset parentOffset, double scale) {
-  var d = parentOffset.dx + (scale * offset.dx);
-  return d < 0 ? 0 : d;
-}
+double calculateLeft(Offset offset, Offset parentOffset, double scale) =>
+    parentOffset.dx + (scale * offset.dx);
 
 double calculateTop(Offset offset, Offset parentOffset, double scale) {
   var d = parentOffset.dy + (scale * offset.dy);
