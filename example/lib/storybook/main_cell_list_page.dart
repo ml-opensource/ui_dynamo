03583a410365a56807cfcd0b8d4bf422c0568a57
fuzzagrosner <andrew@fuzzproductions.com>
2020-05-20 12:42:13 -0400

    [Example] main_cell_list_page.dart gets props.

--- example/lib/storybook/main_cell_list_page.dart
+++ example/lib/storybook/main_cell_list_page.dart
@@ -1,12 +1,25 @@
 import 'package:example/widgets/cells.dart';
 import 'package:flutter_storybook/flutter_storybook.dart';
 
+const PropGroup item1 = PropGroup('Item 1', 'item-1');
+const PropGroup item2 = PropGroup('Item 2', 'item-2');
+
 StoryBookPage buildMainCellListPage() => StoryBookPage.of(
       title: 'Main Cell List',
       child: (context) => MainCellList(
         items: [
-          MainCellItem('A', 'Item 1', 'Item Subtitle', 10),
-          MainCellItem('B', 'Item 2', 'Item Subtitle', 20),
+          MainCellItem(
+            context.props.text('Avatar', 'A', group: item1),
+            context.props.text('Title', 'Item 1', group: item1),
+            context.props.text('Subtitle', 'Item Subtitle', group: item1),
+            context.props.number('Count', 0, group: item1),
+          ),
+          MainCellItem(
+            context.props.text('Avatar', 'B', group: item2),
+            context.props.text('Title', 'Item 1', group: item2),
+            context.props.text('Subtitle', 'Item Subtitle', group: item2),
+            context.props.number('Count', 20, group: item2),
+          ),
         ],
       ),
     );
